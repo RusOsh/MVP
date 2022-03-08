b50ce9ce42e28a7f446d77b285c717ad8632a443
RusOsh <90300562+RusOsh@users.noreply.github.com>
2022-03-08 12:04:53 +0600

    Add files via upload

---
+++ app/src/main/java/com/rus/mvpcounter/ui/MainActivity.java
@@ -0,0 +1,16 @@
+package com.rus.mvpcounter.ui;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.rus.mvpcounter.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
